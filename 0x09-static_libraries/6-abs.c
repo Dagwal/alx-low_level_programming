57d82bc30c2b8ab4880a375767ccc35b510959db
Dagwal <108694236+Dagwal@users.noreply.github.com>
2022-09-30 22:45:50 +0300

    created

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs  - absolute value of a number
+ * @n:` intiger number
+ * Return: @n.
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * -1);
+}
+else
+{
+return (n);
+}
+}
