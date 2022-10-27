c3544614016aed0c152efd41d71d1207ed2ee3ec
Dagwal <108694236+Dagwal@users.noreply.github.com>
2022-10-27 18:45:39 +0300

    pass

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * flip_bits - Counts the number of bits needed to be flipped to get from one number to another.
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
  * @n: The number.
  * @m: The number to flip n to.
+ *
  * Return: The necessary number of bits to flip to get from n to m.
  */
 
